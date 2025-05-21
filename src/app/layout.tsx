78792e597fbf8be0b6a64ff85c92bdea01a0f0d5
MinJi Ko <101329724+gominzip@users.noreply.github.com>
2025-05-21 11:25:07 +0900

    [FEAT] tailwindcss 기반 디자인 토큰 세팅 (#5)
    * docs: Issue 템플릿 추가
    
    * chore: pretendard subset 폰트 추가
    
    * feat: next local font 적용
    
    * feat: color 토큰 추가
    
    * feat: reset.css 추가
    
    * feat: typography 유틸리티 클래스 추가
    
    * refactor: css 파일 import 구문 수정
    
    * chore: 디자인 토큰 적용 테스트용 버튼 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,18 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/reset.css";
+import "@/styles/global.css";
 import Providers from "./providers";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
---
+++ src/fonts/local-font.ts
@@ -0,0 +1,39 @@
+import localFont from "next/font/local";
+
+/*
+Copyright (c) 2021 Kil Hyung-jin, with Reserved Font Name Pretendard.
+https://github.com/orioncactus/pretendard
+
+This Font Software is licensed under the SIL Open Font License, Version 1.1.
+This license is copied below, and is also available with a FAQ at:
+http://scripts.sil.org/OFL
+*/
+
+const pretendard = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Pretendard-Bold.subset.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard-SemiBold.subset.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard-Medium.subset.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard-Regular.subset.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-pretendard",
+  display: "swap",
+});
+
+export default pretendard;
