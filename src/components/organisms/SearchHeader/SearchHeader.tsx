0d723c0c37188af63205641078f7ba347a479a57
gominzip <ahaminji@naver.com>
2025-05-24 17:14:22 +0900

    feat: SearchHeader organism 컴포넌트 생성

---
+++ src/components/organisms/SearchHeader/SearchHeader.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import IconButton from "@/components/molecules/IconButton/IconButton";
+import SearchForm from "@/components/molecules/SearchForm/SearchForm";
+import { cn } from "@/utils/style";
+import Icon from "@/components/atoms/Icon/Icon";
+
+interface SearchHeaderProps {
+  onSearch: (value: string) => void;
+  placeholder?: string;
+  className?: string;
+}
+
+export default function SearchHeader({
+  onSearch,
+  placeholder = "검색어를 입력하세요",
+  className,
+}: SearchHeaderProps) {
+  const router = useRouter();
+
+  return (
+    <header
+      className={cn(
+        "flex items-center gap-[12px] w-full px-[16px] pt-[60px] pb-[16px] bg-surface-normal-bg01 border-b border-brand-primary-main",
+        className
+      )}
+    >
+      <IconButton
+        startIcon={<Icon icon="Back" size={20} />}
+        onClick={() => router.back()}
+      />
+      <div className="flex-1">
+        <SearchForm onSearch={onSearch} placeholder={placeholder} />
+      </div>
+    </header>
+  );
+}
