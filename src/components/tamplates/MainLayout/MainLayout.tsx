d9c69a04effd6c00bf4c132bdc3542f60a82e31d
gominzip <ahaminji@naver.com>
2025-05-24 17:16:04 +0900

    feat: MainLayout 컴포넌트 생성

---
+++ src/components/tamplates/MainLayout/MainLayout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  return (
+    <main className="mx-auto w-full max-w-[600px] min-h-screen flex flex-col items-center bg-surface-normal-bg02">
+      {children}
+    </main>
+  );
+}
