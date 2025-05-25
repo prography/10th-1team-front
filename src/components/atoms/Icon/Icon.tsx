6c15d6d44fee4bf4981f6abf5f871393549d3bc8
gominzip <ahaminji@naver.com>
2025-05-25 22:55:07 +0900

    refactor: Icon 컴포넌트 불필요한 prop 제거

--- src/components/atoms/Icon/Icon.tsx
+++ src/components/atoms/Icon/Icon.tsx
@@ -5,23 +5,19 @@ export type IconName = keyof typeof icons;
 interface IconProps {
   icon: IconName;
   size?: number;
-  color?: string;
   fill?: string;
   stroke?: string;
   strokeWidth?: number;
   className?: string;
-  style?: React.CSSProperties;
 }
 
 export default function Icon({
   icon,
   size = 24,
-  color,
   fill,
   stroke,
   strokeWidth,
   className,
-  style,
 }: IconProps) {
   const IconComponent = icons[icon];
   return (
@@ -29,7 +25,6 @@ export default function Icon({
       width={size}
       height={size}
       className={className}
-      style={{ color, fill, stroke, strokeWidth, ...style }}
       fill={fill}
       stroke={stroke}
       strokeWidth={strokeWidth}
