48f27a1d841eb595f14092fdacd76ce6bd052c0a
gominzip <ahaminji@naver.com>
2025-05-25 22:39:26 +0900

    refactor: svg path 문자열 포맷팅

--- src/components/atoms/Icon/icons/HomeCircle.tsx
+++ src/components/atoms/Icon/icons/HomeCircle.tsx
@@ -12,16 +12,30 @@ export default function HomeCircle(props: SVGProps<SVGSVGElement>) {
       <g clipPath="url(#clip0_394_5663)">
         <circle cx="12" cy="12" r="12" fill={props.fill || "white"} />
         <path
-          d="M12 5L17.6402 9.70015C17.8682 9.89015 18 10.1716 18 10.4684L18 18L14 18L14 15.75L14 14.5C14 13.9477 13.5523 13.5 13 13.5L11 13.5C10.4477 13.5 10 13.9477 10 14.5L10 18L6 18L6 10.4684C6 10.1716 6.13182 9.89015 6.35982 9.70015L12 5Z"
+          d="M12 5
+            L17.6402 9.70015
+            C17.8682 9.89015 18 10.1716 18 10.4684
+            L18 18
+            L14 18
+            L14 15.75
+            L14 14.5
+            C14 13.9477 13.5523 13.5 13 13.5
+            L11 13.5
+            C10.4477 13.5 10 13.9477 10 14.5
+            L10 18
+            L6 18
+            L6 10.4684
+            C6 10.1716 6.13182 9.89015 6.35982 9.70015
+            L12 5Z"
           stroke={props.stroke || "#1A1A1A"}
           strokeLinecap="round"
           strokeLinejoin="round"
         />
       </g>
       <defs>
-        <clipPathh id="clip0_394_5663">
+        <clipPath id="clip0_394_5663">
           <rect width="24" height="24" fill={props.fill || "white"} />
-        </clipPathh>
+        </clipPath>
       </defs>
     </svg>
   );
