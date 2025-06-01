69d00a4ebd4c080303a3c23397d72a0f2b25ddb9
gominzip <ahaminji@naver.com>
2025-06-02 02:13:17 +0900

    feat: List, ListItem atom 컴포넌트 추가

---
+++ src/components/atoms/List/listItemVariants.ts
@@ -0,0 +1,16 @@
+import { cva } from "class-variance-authority";
+
+export const listItemVariants = cva("flex justify-between", {
+  variants: {
+    variant: {
+      "recent-search":
+        "p-[16px] hover:bg-surface-normal-container-b10 border-b-[0.5px] border-border-normal-lowemp",
+      "auto-complete":
+        "flex-col p-[16px] hover:bg-surface-normal-container-b10 border-b-[0.5px] border-border-normal-lowemp",
+      "search-result": "p-[16px] border-b-[0.5px] border-border-normal-lowemp",
+    },
+  },
+  defaultVariants: {
+    variant: "auto-complete",
+  },
+});
