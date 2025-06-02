0d0e9af203510a19bcf7d8084cf7bfd0daed3f44
gominzip <ahaminji@naver.com>
2025-06-02 16:59:59 +0900

    chore: button, listitem variant 일부 수정

--- src/components/atoms/List/listItemVariants.ts
+++ src/components/atoms/List/listItemVariants.ts
@@ -3,7 +3,7 @@ import { cva } from "class-variance-authority";
 export const listItemVariants = cva("flex justify-between", {
   variants: {
     variant: {
-      "recent-search":
+      "history-search":
         "p-[16px] hover:bg-surface-normal-container-b10 border-b-[0.5px] border-border-normal-lowemp",
       "auto-complete":
         "flex-col p-[16px] hover:bg-surface-normal-container-b10 border-b-[0.5px] border-border-normal-lowemp",
