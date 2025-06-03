641c6276e2abf2f91edb1f255f4cc146444e55c1
gominzip <ahaminji@naver.com>
2025-06-03 17:58:06 +0900

    fix: 동적으로 구성된 클래스 이름을 정적 클래스 매핑 방식으로 수정

--- src/components/atoms/Divider/Divider.tsx
+++ src/components/atoms/Divider/Divider.tsx
@@ -2,10 +2,30 @@ import { cn } from "@/utils/cn";
 import { dividerVariants } from "./dividerVariants";
 import { type VariantProps } from "class-variance-authority";
 
+type OrientationType = "horizontal" | "vertical";
+type Thickness = 1 | 2 | 4 | 8 | 12;
+
+const thicknessClassMap: Record<OrientationType, Record<Thickness, string>> = {
+  horizontal: {
+    1: "h-[1px]",
+    2: "h-[2px]",
+    4: "h-[4px]",
+    8: "h-[8px]",
+    12: "h-[12px]",
+  },
+  vertical: {
+    1: "w-[1px]",
+    2: "w-[2px]",
+    4: "w-[4px]",
+    8: "w-[8px]",
+    12: "w-[12px]",
+  },
+};
+
 type DividerProps = {
-  orientation?: "horizontal" | "vertical";
+  orientation?: OrientationType;
   className?: string;
-  thickness?: number;
+  thickness?: Thickness;
 } & VariantProps<typeof dividerVariants>;
 
 export default function Divider({
@@ -19,8 +39,8 @@ export default function Divider({
       className={cn(
         dividerVariants({ variant }),
         orientation === "horizontal"
-          ? `w-full h-[${thickness}px]`
-          : `h-full w-[${thickness}px]`,
+          ? `${thicknessClassMap[orientation][thickness]} w-full`
+          : `${thicknessClassMap[orientation][thickness]} h-full`,
         className
       )}
     />
