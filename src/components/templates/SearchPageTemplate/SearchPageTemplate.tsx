a1073083858d7cdd95c52270c62eb478a72e8433
gominzip <ahaminji@naver.com>
2025-06-04 13:28:06 +0900

    refactor: Discriminated Union 적용으로 mode에 따라 props 타입 분기

--- src/components/templates/SearchPageTemplate/SearchPageTemplate.tsx
+++ src/components/templates/SearchPageTemplate/SearchPageTemplate.tsx
@@ -2,31 +2,50 @@ import SearchHeader from "@/components/organisms/SearchHeader/SearchHeader";
 import SearchListSortTab from "@/components/organisms/SearchListSortTab/SearchListSortTab";
 import SearchHistoryHeader from "@/components/organisms/SearchHistoryHeader/SearchHistoryHeader";
 import SearchFilterTab from "@/components/organisms/SearchFilterTab/SearchFilterTab";
-import SearchListContainer from "@/components/organisms/SearchListContainer/SearchListContainer";
 import Divider from "@/components/atoms/Divider/Divider";
+import {
+  SearchAutoCompleteList,
+  SearchHistoryList,
+  SearchResultList,
+} from "@/components/organisms/SearchListContainer/lists";
 
 import type {
   AutoCompleteItem,
-  SearchMode,
   SearchRecentItem,
   SearchResultItem,
 } from "@/types/search";
 
-interface SearchPageTemplateProps {
-  mode: SearchMode;
-  items: SearchResultItem[] | AutoCompleteItem[] | SearchRecentItem[];
+export type ModePropsMap = {
+  history: {
+    items: SearchRecentItem[];
+    onItemClick: (item: SearchRecentItem) => void;
+  };
+  autocomplete: {
+    items: AutoCompleteItem[];
+    onItemClick: (item: AutoCompleteItem) => void;
+  };
+  results: {
+    items: SearchResultItem[];
+    onItemClick: (item: SearchResultItem) => void;
+  };
+};
+
+export type Mode = keyof ModePropsMap;
+
+type SharedProps = {
   query: string;
   setQuery: (value: string) => void;
   onSearch: (value: string) => void;
   onDeleteHistory: (id: string) => void;
   onClearHistory: () => void;
-  onItemClick: (
-    item: SearchResultItem | AutoCompleteItem | SearchRecentItem
-  ) => void;
   onBack: () => void;
   observerRef: React.RefObject<HTMLDivElement>;
   isLoading: boolean;
-}
+};
+
+type SearchPageTemplateProps = {
+  [K in Mode]: SharedProps & { mode: K } & ModePropsMap[K];
+}[Mode];
 
 export default function SearchPageTemplate({
   mode,
@@ -43,6 +62,7 @@ export default function SearchPageTemplate({
 }: SearchPageTemplateProps) {
   return (
     <div className="flex flex-col h-full w-full bg-surface-normal-container0">
+      {/* 헤더 */}
       <div className="sticky top-0 z-10">
         <SearchHeader
           query={query}
@@ -56,25 +76,45 @@ export default function SearchPageTemplate({
         )}
         {mode === "results" && (
           <>
-            <SearchFilterTab onOpenFilterModal={() => {}} />
+            <SearchFilterTab
+              onOpenFilterModal={() => {
+                /* TODO: 필터 모달 추가 */
+              }}
+            />
             <Divider />
-            <SearchListSortTab openSortSheet={() => {}} />
+            <SearchListSortTab
+              openSortSheet={() => {
+                /* TODO: 정렬 모달 추가 */
+              }}
+            />
           </>
         )}
       </div>
 
+      {/* 리스트 */}
       <div className="flex-1 min-h-0 overflow-y-auto">
         {isLoading && <p className="py-4 text-center">불러오는 중...</p>}
 
-        <SearchListContainer
-          mode={mode}
-          query={query}
-          items={items}
-          onItemClick={onItemClick}
-          onDeleteHistory={onDeleteHistory}
-        />
-
-        {mode === "results" && <div ref={observerRef} className="h-[1px]" />}
+        {mode === "history" && (
+          <SearchHistoryList
+            items={items}
+            onItemClick={onItemClick}
+            onDeleteItem={onDeleteHistory}
+          />
+        )}
+        {mode === "autocomplete" && (
+          <SearchAutoCompleteList
+            items={items}
+            query={query}
+            onItemClick={onItemClick}
+          />
+        )}
+        {mode === "results" && (
+          <>
+            <SearchResultList items={items} onItemClick={onItemClick} />
+            {!isLoading && <div ref={observerRef} className="h-[1px]" />}
+          </>
+        )}
       </div>
     </div>
   );
