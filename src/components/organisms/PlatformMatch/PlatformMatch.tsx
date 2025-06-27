1e1f7d7363219cae6e6575ff16feb08d59323320
ZeroIRC <save3172@gmail.com>
2025-06-27 23:42:33 +0900

    feat: 플랫폼 매치 관련 컴포넌트 구현

---
+++ src/components/organisms/PlatformMatch/PlatformMatch.tsx
@@ -0,0 +1,44 @@
+import Tabs from '@/components/organisms/Tabs/Tabs';
+import PlatformVote from '@/components/organisms/PlatformVote/PlatformVote';
+import PlatformMatchResult from '@/components/organisms/PlatformMatchResult/PlatformMatchResult';
+import PlatformMatchVoteModal from '@/components/organisms/PlatformMatchVoteModal/PlatformMatchVoteModal';
+import { useState } from 'react';
+
+export default function PlatformMatch() {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleVoteClick = () => {
+        setModalOpen(true);
+    };
+    
+    const tabItems = [
+        {
+            label: '투표 참여하기',
+            value: 'vote',
+            content: <PlatformVote handleVoteClick={handleVoteClick} />
+        },
+        {
+            label: '결과 확인하기',
+            value: 'result',
+            content: <PlatformMatchResult handleVoteClick={handleVoteClick} />
+        }
+    ]
+    return (
+        <div className='bg-surface-normal-bg01 py-[16px] w-full max-w-full overflow-hidden'>
+            <div className='px-[16px] gap-[12px] flex flex-col'>
+                <h2 className="body-m-semibold ">플랫폼 매치</h2>
+                <div className='gap-[4px] flex flex-col'>
+                    <p className='body-l-semibold text-texticon-onnormal-highemp'>어느쪽 리뷰가 더 신뢰가나요?</p>
+                    <p className='caption-m-regular text-texticon-onnormal-midemp'>더 유용한 정보를 제공한 플랫폼과 이유를 투표해주세요</p>
+                </div>
+            </div>
+            <Tabs items={tabItems} />
+            <PlatformMatchVoteModal
+                open={modalOpen}
+                onClose={() => setModalOpen(false)}
+                onSubmit={() => setModalOpen(false)}
+            />
+        </div>
+    )
+    
+} 
---
+++ src/components/organisms/PlatformVote/PlatformVote.tsx
@@ -0,0 +1,25 @@
+import Button from '@/components/atoms/Button/Button';
+import Image from 'next/image';
+import React from 'react';
+
+
+export default function PlatformVote( {handleVoteClick}: {handleVoteClick: () => void} ) {
+    
+    return (
+        <div className='px-[16px] py-[14px] flex flex-col gap-[12px]'>
+                <Image
+                    src="/images/PlatformVote.svg"
+                    alt="platform-vote"
+                    width={600}
+                    height={184}
+                />
+            <Button 
+                variant={'primary'} 
+                className='w-full h-[56px]'
+                onClick={handleVoteClick}
+            >
+                투표하기
+            </Button>
+        </div>
+    )
+} 
