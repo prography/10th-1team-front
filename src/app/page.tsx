53b0e3a117500785e9a72a48b4c6852f9e339911
gominzip <ahaminji@naver.com>
2025-06-28 01:56:49 +0900

    feat: 메인 페이지 퍼블리싱 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
+import MainLayout from "@/components/templates/MainLayout/MainLayout";
+import MainPageTemplate from "@/components/templates/MainPageTemplate/MainPageTemplate";
+
 export default function Home() {
   return (
-    <div>
-      <button className="w-[328px] h-[56px] rounded-[8px] button-l-semibold bg-button-primary-bg_main text-button-primary-text_default">
-        Button
-      </button>
-      <button className="w-[328px] h-[56px] rounded-[8px] button-l-semibold bg-button-primary-bg_pressed text-button-primary-text_disabled">
-        Button
-      </button>
-    </div>
+    <MainLayout>
+      <MainPageTemplate />
+    </MainLayout>
   );
 }
