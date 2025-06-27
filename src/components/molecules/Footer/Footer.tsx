b6820e0d19fd5913c19c3444405930aa3ff72561
gominzip <ahaminji@naver.com>
2025-06-28 01:13:27 +0900

    feat: 메인 페이지 Footer 구현

---
+++ src/components/molecules/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import Icon from "@/components/atoms/Icon/Icon";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="flex flex-col items-start bg-surface-normal-bg0 px-[16px] pt-[28px] pb-[112px] gap-[20px]">
+      <Icon icon="ServiceLogo" size={24} />
+      <div className="flex gap-[24px] text-texticon-onnormal-midemp caption-m-regular">
+        {/* TODO: 기획 후 링크 추가 필요 */}
+        <Link href="/">팀 소개</Link>
+        <Link href="/">개인정보취급방침</Link>
+        <Link href="/">이용약관</Link>
+        <Link href="/">질문/건의</Link>
+      </div>
+    </div>
+  );
+}
