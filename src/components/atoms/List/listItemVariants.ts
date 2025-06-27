43c438bca11fc9f73e3f97bd0d4fe70c62848d63
ZeroIRC <save3172@gmail.com>
2025-06-27 23:41:52 +0900

    feat: listItemVariants platform-vote 추가

--- src/components/atoms/List/listItemVariants.ts
+++ src/components/atoms/List/listItemVariants.ts
@@ -8,6 +8,7 @@ export const listItemVariants = cva("flex justify-between", {
       "auto-complete":
         "flex-col p-[16px] hover:bg-surface-normal-container-b10 border-b-[0.5px] border-border-normal-lowemp",
       "search-result": "p-[16px] border-b-[0.5px] border-border-normal-lowemp",
+      "platform-vote": "bg-button-neutral-bg_default border border-button-neutral-border text-button-neutral-text_default hover:bg-surface-normal-container-b50 hover:text-texticon-onnormal-main-500 hover:border-brand-primary-main",
     },
   },
   defaultVariants: {
