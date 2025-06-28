389864ed86c89a2f7c4d566d1dc8f996c095ad22
ZeroIRC <save3172@gmail.com>
2025-06-28 21:01:59 +0900

    fix: 추가된 SVG 아이콘파일 카멜케이스로 변경

--- src/components/atoms/Icon/icons/GraphicsKakao.tsx
+++ src/components/atoms/Icon/icons/GraphicsKakao.tsx
@@ -10,19 +10,19 @@ export default function GraphicsNaver(props: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
     >
       <g filter="url(#filter0_diiii_930_14264)">
-        <g clip-path="url(#clip0_930_14264)">
+        <g clipPath="url(#clip0_930_14264)">
           <rect width="50" height="50" rx="6" fill="#FAE100" />
           <g filter="url(#filter1_di_930_14264)">
             <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+              fillRule="evenodd"
+              clipRule="evenodd"
               d="M24.9997 6.16699C32.5475 6.16699 38.6663 12.2858 38.6663 19.8337C38.6663 24.917 35.9997 28.0837 30.7497 34.0003C28.9888 35.9848 27.2994 40.1408 26.2129 43.2051C26.032 43.7152 25.5062 43.948 24.9997 43.9137C24.4932 43.948 23.9673 43.7152 23.7865 43.2051C22.7 40.1408 21.0105 35.9848 19.2497 34.0003C13.9997 28.0837 11.333 24.917 11.333 19.8337C11.333 12.2858 17.4518 6.16699 24.9997 6.16699ZM25.2497 14.8636C25.1658 14.8636 25.0824 14.8661 24.9997 14.8706C24.9169 14.8661 24.8336 14.8636 24.7497 14.8636C22.2644 14.8636 20.2497 16.8783 20.2497 19.3636C20.2497 21.8489 22.2644 23.8636 24.7497 23.8636C24.8336 23.8636 24.9169 23.861 24.9997 23.8564C25.0824 23.861 25.1658 23.8636 25.2497 23.8636C27.735 23.8636 29.7497 21.8489 29.7497 19.3636C29.7497 16.8783 27.735 14.8636 25.2497 14.8636Z"
               fill="#007DFF"
             />
             <path
               d="M25 6.41699C32.4096 6.41717 38.416 12.4243 38.416 19.834C38.4159 24.8063 35.826 27.9022 30.5625 33.834C29.6572 34.8543 28.7825 36.4168 28.002 38.0918C27.2192 39.7716 26.5222 41.585 25.9775 43.1211C25.8453 43.4939 25.4487 43.6933 25.0166 43.6641L25 43.6631L24.9824 43.6641C24.5505 43.6932 24.1546 43.4938 24.0225 43.1211C23.4778 41.5849 22.7799 39.7717 21.9971 38.0918C21.2165 36.4169 20.3418 34.8542 19.4365 33.834C14.1732 27.9023 11.5831 24.8063 11.583 19.834L11.5869 19.4873C11.7706 12.2376 17.706 6.41699 25 6.41699ZM25.25 14.6133C25.166 14.6133 25.0824 14.6158 25 14.6201C24.9175 14.6158 24.8341 14.6133 24.75 14.6133C22.1268 14.6133 20.0002 16.7401 20 19.3633C20 21.9866 22.1266 24.1133 24.75 24.1133C24.8344 24.1133 24.9178 24.1107 25 24.1064C25.0821 24.1107 25.1657 24.1133 25.25 24.1133C27.8732 24.1131 30 21.9865 30 19.3633C29.9998 16.7402 27.8731 14.6135 25.25 14.6133Z"
               stroke="white"
-              stroke-width="0.5"
+              strokeWidth="0.5"
             />
           </g>
         </g>
@@ -43,9 +43,9 @@ export default function GraphicsNaver(props: SVGProps<SVGSVGElement>) {
           width="59"
           height="60"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
@@ -162,9 +162,9 @@ export default function GraphicsNaver(props: SVGProps<SVGSVGElement>) {
           width="43.333"
           height="53.75"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
@@ -216,8 +216,8 @@ export default function GraphicsNaver(props: SVGProps<SVGSVGElement>) {
           y2="52.5"
           gradientUnits="userSpaceOnUse"
         >
-          <stop offset="0.0810824" stop-color="white" />
-          <stop offset="1" stop-color="#6D96FF" />
+          <stop offset="0.0810824" stopColor="white" />
+          <stop offset="1" stopColor="#6D96FF" />
         </linearGradient>
         <clipPath id="clip0_930_14264">
           <rect width="50" height="50" rx="6" fill="white" />
--- src/components/atoms/Icon/icons/GraphicsNaver.tsx
+++ src/components/atoms/Icon/icons/GraphicsNaver.tsx
@@ -10,7 +10,7 @@ export default function GraphicsNaver(props: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
     >
       <g filter="url(#filter0_dii_930_14263)">
-        <g clip-path="url(#clip0_930_14263)">
+        <g clipPath="url(#clip0_930_14263)">
           <rect y="0.212891" width="50" height="50" rx="6" fill="white" />
           <g filter="url(#filter1_d_930_14263)">
             <rect
@@ -29,7 +29,7 @@ export default function GraphicsNaver(props: SVGProps<SVGSVGElement>) {
             <path
               d="M19.666 6.96289C26.3393 6.96289 31.7498 12.3726 31.75 19.0459C31.75 20.5159 31.1539 22.3724 30.2031 24.3887C29.2553 26.3985 27.9679 28.5408 26.6133 30.5732C23.9044 34.6376 20.9398 38.2448 19.9238 39.4541C19.8279 39.5681 19.668 39.5686 19.5713 39.4551C18.5437 38.2485 15.5389 34.6407 12.792 30.5752C11.4184 28.5422 10.1125 26.4001 9.15137 24.3896C8.18703 22.3725 7.58301 20.5156 7.58301 19.0459C7.58318 12.3727 12.9928 6.96307 19.666 6.96289Z"
               stroke="#CEFFE4"
-              stroke-width="0.5"
+              strokeWidth="0.5"
             />
           </g>
           <g filter="url(#filter3_d_930_14263)">
@@ -40,7 +40,7 @@ export default function GraphicsNaver(props: SVGProps<SVGSVGElement>) {
             <path
               d="M18.373 23.2959V19.207L21.1309 23.1885L21.2051 23.2959H24.25V14.1289H20.96V18.2188L18.2021 14.2373L18.127 14.1289H15.083V23.2959H18.373Z"
               stroke="#019944"
-              stroke-width="0.5"
+              strokeWidth="0.5"
             />
           </g>
         </g>
@@ -61,9 +61,9 @@ export default function GraphicsNaver(props: SVGProps<SVGSVGElement>) {
           width="58"
           height="58"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
@@ -132,9 +132,9 @@ export default function GraphicsNaver(props: SVGProps<SVGSVGElement>) {
           width="38.167"
           height="16"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
@@ -167,9 +167,9 @@ export default function GraphicsNaver(props: SVGProps<SVGSVGElement>) {
           width="42.667"
           height="51.0771"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
@@ -220,9 +220,9 @@ export default function GraphicsNaver(props: SVGProps<SVGSVGElement>) {
           width="17.667"
           height="17.6667"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
@@ -256,8 +256,8 @@ export default function GraphicsNaver(props: SVGProps<SVGSVGElement>) {
           y2="50.2129"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="white" />
-          <stop offset="1" stop-color="#4D7FFF" />
+          <stop stopColor="white" />
+          <stop offset="1" stopColor="#4D7FFF" />
         </linearGradient>
         <clipPath id="clip0_930_14263">
           <rect y="0.212891" width="50" height="50" rx="6" fill="white" />
