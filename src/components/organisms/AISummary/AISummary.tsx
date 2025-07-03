850341fbee3d740f84a36fe26f655cb678f1dd2a
ZeroIRC <save3172@gmail.com>
2025-07-04 02:04:34 +0900

    feat: AI요약 추가 구현

--- src/components/organisms/AISummary/AISummary.tsx
+++ src/components/organisms/AISummary/AISummary.tsx
@@ -1,48 +1,43 @@
 "use client";
 
 import Tabs from "@/components/organisms/Tabs/Tabs";
-import Image from "next/image";
+import BarGraph from "@/components/molecules/BarGraph/BarGraph";
 
-const tabItems = [
-  {
-    label: "음식만족도",
-    value: "food",
-    content: (
-      <div className="flex justify-center items-center w-full">
-        <Image src="/images/graph.svg" alt="food" width={328} height={260} />
-      </div>
-    ),
-  },
-  {
-    label: "가격만족도",
-    value: "price",
-    content: (
-      <div className="flex justify-center items-center w-full">
-        <Image src="/images/graph.svg" alt="food" width={328} height={260} />
-      </div>
-    ),
-  },
-  {
-    label: "서비스",
-    value: "service",
-    content: (
-      <div className="flex justify-center items-center w-full">
-        <Image src="/images/graph.svg" alt="food" width={328} height={260} />
-      </div>
-    ),
-  },
-  {
-    label: "분위기",
-    value: "atmosphere",
-    content: (
-      <div className="flex justify-center items-center w-full">
-        <Image src="/images/graph.svg" alt="food" width={328} height={260} />
-      </div>
-    ),
-  },
-];
+interface AISummaryProps {
+  summaryAi: string;
+  strengths: Array<{
+    description: string;
+    kakao_rate: number;
+    naver_rate: number;
+  }>;
+}
 
-export default function AISummary() {
+export default function AISummary({ summaryAi, strengths }: AISummaryProps) {
+  const tabItems = strengths.map((strength) => ({
+    label: strength.description,
+    value: strength.description,
+    content: (
+      <div className="flex flex-col w-full gap-[24px]">
+        {summaryAi ? (
+          <p className="body-m-semibold px-[16px] pt-[12px] text-left">
+            {summaryAi}
+          </p>
+        ) : (
+          <p className="body-m-semibold px-[16px] pt-[12px] text-texticon-onnormal-lowestemp text-left">
+            AI요약을 위한 리뷰 데이터가 부족해요
+          </p>
+        )}
+        <div className="flex justify-center">
+          <BarGraph
+            leftLabel="카카오 맵"
+            rightLabel="네이버지도"
+            leftPercent={Math.round(strength.kakao_rate * 100)}
+            rightPercent={Math.round(strength.naver_rate * 100)}
+          />
+        </div>
+      </div>
+    ),
+  }));
   return (
     <div className="flex flex-col py-[24px] w-full bg-surface-normal-bg01">
       <p className="body-m-semibold px-[16px]">AI 요약</p>
