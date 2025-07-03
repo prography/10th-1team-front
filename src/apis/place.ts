941ac684cc4f7fc70f41fdaf420e2a803cce46db
ZeroIRC <save3172@gmail.com>
2025-07-04 02:06:33 +0900

    feat: place api 추가

--- src/apis/place.ts
+++ src/apis/place.ts
@@ -1,11 +1,89 @@
 import { API } from ".";
 
+interface PlacePlatformMatchSummaryResponse {
+  data: {
+    total: number;
+    is_user_voted: boolean;
+  };
+}
+interface Review {
+  id: string;
+  author: string;
+  review_platform: "NAVER" | "KAKAO";
+  author_image_url: string | null;
+  registered_at: string;
+  contents: string;
+  star_rating: number | null;
+}
+interface PlaceReviewResponse {
+  data: {
+    name: string;
+    road_address_name: string;
+    total_count: number;
+    kakao_review_count: number;
+    naver_review_count: number;
+    kakao_place_uri: string;
+    naver_place_uri: string;
+    reviews: Review[];
+  };
+}
+
 export const getPlaceDetail = async (id: string) => {
   try {
     const response = await API.get(`/restaurant/detail/${id}`);
     return response.data.data;
   } catch (error) {
     console.error("Place 상세 정보 조회 실패:", error);
+    throw error;
+  }
+};
+
+export const patchPlatformMatchVote = async (
+  id: string,
+  data: {
+    platform: "KAKAO" | "NAVER";
+    reasons: string[];
+  }
+) => {
+  try {
+    const response = await API.patch(`/vote/submit/${id}`, data);
+    return response.data.data;
+  } catch (error) {
+    console.error("Platform 투표하기 실패:", error);
+    throw error;
+  }
+};
+
+export const getPlatformMatchResult = async (placeId: string) => {
+  try {
+    const response = await API.get(`/vote/${placeId}`);
+    return response.data.data;
+  } catch (error) {
+    console.error("Platform 투표 결과 조회 실패:", error);
+    throw error;
+  }
+};
+
+export const getPlaceReview = async (placeId: string) => {
+  try {
+    const { data } = await API.get<PlaceReviewResponse>(
+      `/restaurant/reviews/${placeId}`
+    );
+    return data.data;
+  } catch (error) {
+    console.error("Place 리뷰 조회 실패:", error);
+    throw error;
+  }
+};
+
+export const getPlacePlatformMatchSummary = async (placeId: string) => {
+  try {
+    const { data } = await API.get<PlacePlatformMatchSummaryResponse>(
+      `/vote/summary/${placeId}`
+    );
+    return data.data;
+  } catch (error) {
+    console.error("Place 플랫폼 매치 요약 조회 실패:", error);
     throw error;
   }
 };
