39ac36f5158386e3fcac225a561076ab8f4e988f
gominzip <ahaminji@naver.com>
2025-07-04 19:05:17 +0900

    feat: SSR, CSR에 대응하여 사용자 정보를 가져오는 getMyInfo 함수 추가

---
+++ src/apis/user.ts
@@ -0,0 +1,15 @@
+import { UserInfoResponse } from "@/types/user";
+import { authProxyAPI } from "./customAxios";
+import { fetchWithAuth } from "./fetchWithAuth";
+
+export const getMyInfo = async () => {
+  // Server 컴포넌트에서 실행되는 경우
+  if (typeof window === "undefined") {
+    const data = await fetchWithAuth<UserInfoResponse>("/users/me");
+    return data.data;
+  } else {
+    // Client 컴포넌트에서 실행되는 경우 (프록시 사용)
+    const { data } = await authProxyAPI.get<UserInfoResponse>(`/users/me`);
+    return data.data;
+  }
+};
