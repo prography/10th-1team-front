4598a1c5eeb00d0f70ec32b71ba0b381d2ac3148
gominzip <ahaminji@naver.com>
2025-07-04 21:16:42 +0900

    feat: 메인 페이지 구현

--- src/app/main/MainPage.tsx
+++ src/app/main/MainPage.tsx
@@ -3,15 +3,28 @@
 import { useRecommendListQuery } from "@/hooks/queries/useRecommendListQuery";
 import MainPageTemplate from "@/components/templates/MainPageTemplate/MainPageTemplate";
 import MainLayout from "@/components/templates/MainLayout/MainLayout";
+import { logout } from "@/apis/login";
+import useUserStore from "@/store/useUserStore";
 
 export default function MainPage() {
+  const user = useUserStore((state) => state.user);
+  const handleLogout = async () => {
+    await logout();
+    useUserStore.getState().clearUser();
+  };
+
   const { data: recommendList, isLoading } = useRecommendListQuery({
     size: 3,
   });
 
   return (
     <MainLayout>
-      <MainPageTemplate items={recommendList ?? []} isLoading={isLoading} />
+      <MainPageTemplate
+        user={user}
+        items={recommendList ?? []}
+        isLoading={isLoading}
+        onLogout={handleLogout}
+      />
     </MainLayout>
   );
 }
