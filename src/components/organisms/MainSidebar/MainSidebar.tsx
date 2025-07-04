a27aa17752611ffe6e45c3f3187779e4fe84dda5
gominzip <ahaminji@naver.com>
2025-07-04 21:11:35 +0900

    feat: 메인 사이드바 UI 1차 구현

---
+++ src/components/organisms/MainSidebar/MainSidebar.tsx
@@ -0,0 +1,145 @@
+import IconButton from "@/components/molecules/IconButton/IconButton";
+import Icon from "@/components/atoms/Icon/Icon";
+import Footer from "@/components/molecules/Footer/Footer";
+import Divider from "@/components/atoms/Divider/Divider";
+import Button from "@/components/atoms/Button/Button";
+import { useEffect } from "react";
+import { allowScroll, preventScroll } from "@/utils/modal";
+import type { UserInfo } from "@/types/user";
+import Image from "next/image";
+
+interface MainSidebarProps {
+  onClose: () => void;
+  onStart: () => void;
+  onLogout: () => void;
+  user: UserInfo | null;
+}
+
+export default function MainSidebar({
+  onClose,
+  onStart,
+  onLogout,
+  user,
+}: MainSidebarProps) {
+  useEffect(() => {
+    const prevScrollY = preventScroll();
+    return () => {
+      allowScroll(prevScrollY);
+    };
+  }, []);
+
+  return (
+    <div className="fixed inset-0 z-50 flex justify-center items-start">
+      <div className="w-full max-w-[600px] h-full overflow-y-auto flex flex-col bg-surface-normal-bg01">
+        <div className="flex justify-end border-b border-border-normal-lowemp pt-[24px] pb-[16px] px-[16px]">
+          <IconButton
+            startIcon={<Icon icon="Exit" size={24} />}
+            onClick={onClose}
+          />
+        </div>
+        {/* 사이드바 내부 콘텐츠들 */}
+        <div className="flex flex-col flex-1 border-b border-border-normal-lowemp">
+          {user ? (
+            <div className="flex flex-col gap-[12px] px-[16px] py-[20px]">
+              <p className="body-l-semibold text-texticon-onnormal-highestemp">
+                <span className="text-texticon-onnormal-main-500 underline underline-offset-3">
+                  {user.nickname}
+                </span>
+                님,
+                <br />
+                다음 레벨 달성까지 얼마 안남았어요!
+              </p>
+              <Image
+                src="/images/TestLevelGraphic.svg"
+                alt="level"
+                width={120}
+                height={120}
+                className="w-full h-full"
+              />
+            </div>
+          ) : (
+            <>
+              <p className="body-l-semibold text-texticon-onnormal-highestemp px-[16px] py-[20px]">
+                리뷰:매치에서
+                <br />더 다양한 가게의 리뷰를 매치해보세요
+              </p>
+              <div className="px-[16px] py-[14px]">
+                <Button
+                  variant="primary"
+                  fullWidth
+                  className="py-[16px]"
+                  onClick={onStart}
+                >
+                  시작하기
+                </Button>
+              </div>
+            </>
+          )}
+
+          {user && (
+            <>
+              <Divider />
+              <section className="flex flex-col justify-between px-[16px] py-[24px] gap-[120px]">
+                <div className="flex flex-col gap-[48px]">
+                  <div className="flex flex-col gap-[16px]">
+                    <h6 className="body-l-semibold text-texticon-onnormal-highestemp">
+                      활동 데이터
+                    </h6>
+                    <div className="flex flex-col items-start gap-[12px] text-texticon-onnormal-lowemp body-s-semibold">
+                      <Button variant="text">저장한 가게</Button>
+                      <Button variant="text">투표한 가게</Button>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-[16px]">
+                    <h6 className="body-l-semibold text-texticon-onnormal-highestemp">
+                      알림
+                    </h6>
+                    <div className="flex flex-col items-start gap-[12px] text-texticon-onnormal-lowemp body-s-semibold">
+                      <Button variant="text">알림 내역</Button>
+                      <Button variant="text">알림 설정</Button>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-[16px]">
+                    <h6 className="body-l-semibold text-texticon-onnormal-highestemp">
+                      문의
+                    </h6>
+                    <div className="flex flex-col items-start gap-[12px] text-texticon-onnormal-lowemp body-s-semibold">
+                      <Button variant="text">자주 묻는 질문</Button>
+                      <Button variant="text">1:1 문의하기</Button>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-[16px]">
+                    <h6 className="body-l-semibold text-texticon-onnormal-highestemp">
+                      약관 및 정책
+                    </h6>
+                    <div className="flex flex-col items-start gap-[12px] text-texticon-onnormal-lowemp body-s-semibold">
+                      <Button variant="text">개인정보 처리 방침</Button>
+                      <Button variant="text">이용약관</Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full flex justify-center gap-[28px]">
+                  <Button
+                    variant="text"
+                    className="text-texticon-onnormal-lowemp caption-m-regular"
+                    onClick={onLogout}
+                  >
+                    로그아웃
+                  </Button>
+                  <Button
+                    variant="text"
+                    className="text-texticon-onnormal-lowemp caption-m-regular"
+                  >
+                    회원탈퇴
+                  </Button>
+                </div>
+              </section>
+            </>
+          )}
+        </div>
+
+        <Footer />
+      </div>
+    </div>
+  );
+}
