412670360175c807ad38ac6707ee0d16033e76b1
gominzip <ahaminji@naver.com>
2025-07-14 19:43:25 +0900

    refactor: getMyInfo 에러처리 코드 추가

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -5,8 +5,13 @@ import { fetchWithAuth } from "./fetchWithAuth";
 export const getMyInfo = async () => {
   // Server 컴포넌트에서 실행되는 경우
   if (typeof window === "undefined") {
-    const data = await fetchWithAuth<UserInfoResponse>("/users/me");
-    return data.data;
+    try {
+      const data = await fetchWithAuth<UserInfoResponse>("/users/me");
+      return data.data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
   } else {
     // Client 컴포넌트에서 실행되는 경우 (프록시 사용)
     const { data } = await authProxyAPI.get<UserInfoResponse>(`/users/me`);
