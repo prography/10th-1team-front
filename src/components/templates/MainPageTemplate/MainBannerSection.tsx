ec9086a9bdc721925c93746c0795c003a01054b9
gominzip <ahaminji@naver.com>
2025-07-16 18:27:02 +0900

    chore: 메인 배너 이미지 변경

--- src/components/templates/MainPageTemplate/MainBannerSection.tsx
+++ src/components/templates/MainPageTemplate/MainBannerSection.tsx
@@ -5,7 +5,7 @@ export default function MainBannerSection() {
     <div className="relative w-full px-[16px] py-[12px]">
       <div className="relative w-full h-full aspect-[326/184] bg-gray-200 rounded-lg overflow-hidden">
         <Image
-          src={"/images/MainBanner.webp"}
+          src={"/images/MainBanner.svg"}
           alt={"MainBanner"}
           fill
           sizes="600px"
