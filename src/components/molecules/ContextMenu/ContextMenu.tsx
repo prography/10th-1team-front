c0c66e4e2ff893d761425e76160eae7a42f5328d
gominzip <ahaminji@naver.com>
2025-07-16 15:33:29 +0900

    feat: 드롭다운 스타일의 ContextMenu molecule 컴포넌트 추가

---
+++ src/components/molecules/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,79 @@
+import { JSX, ReactNode, useRef, useState } from "react";
+import MenuItem, { MenuItemProps } from "./MenuItem";
+import Menu from "./Menu";
+import Divider from "@/components/atoms/Divider/Divider";
+import React from "react";
+import { cn } from "@/utils/cn";
+
+interface ContextMenuProps {
+  trigger: (props: JSX.IntrinsicElements["button"]) => ReactNode;
+  items: MenuItemProps[];
+  align?: "left" | "right" | "center";
+  side?: "top" | "bottom";
+  offset?: { x?: number; y?: number };
+  icon?: ReactNode;
+  className?: string;
+}
+
+export default function ContextMenu({
+  trigger,
+  items,
+  align = "left",
+  side = "bottom",
+  offset = { x: 0, y: 0 },
+  icon,
+  className,
+}: ContextMenuProps) {
+  const [open, setOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement>(null);
+
+  const handleToggle = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setOpen((prev) => !prev);
+  };
+
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div
+      className={cn(
+        "relative w-[160px] flex",
+        className,
+        align === "left" && "justify-start",
+        align === "right" && "justify-end"
+      )}
+    >
+      {trigger({
+        ref: triggerRef,
+        onClick: handleToggle,
+      })}
+      {open && (
+        <Menu
+          open={open}
+          anchorEl={triggerRef.current}
+          onClose={handleClose}
+          align={align}
+          side={side}
+          offset={offset}
+        >
+          {items.map((item, i) => (
+            <React.Fragment key={item.label}>
+              <MenuItem
+                label={item.label}
+                onClick={() => {
+                  item.onClick();
+                  handleClose();
+                }}
+                selected={item.selected}
+                icon={icon}
+              />
+              {i !== items.length - 1 && (
+                <Divider thickness={0.5} variant="border" />
+              )}
+            </React.Fragment>
+          ))}
+        </Menu>
+      )}
+    </div>
+  );
+}
---
+++ src/components/molecules/ContextMenu/MenuItem.tsx
@@ -0,0 +1,30 @@
+import { cn } from "@/utils/cn";
+import { ReactNode } from "react";
+
+export interface MenuItemProps {
+  label: string;
+  onClick: () => void;
+  icon?: ReactNode;
+  selected?: boolean;
+}
+
+export default function MenuItem({
+  onClick,
+  label,
+  icon,
+  selected,
+}: MenuItemProps) {
+  return (
+    <div
+      onClick={onClick}
+      className={cn(
+        "flex w-full px-[24px] py-[12px] items-center justify-between cursor-pointer",
+        selected && "text-texticon-onnormal-black body-s-semibold",
+        !selected && "text-texticon-onnormal-highemp body-s-regular"
+      )}
+    >
+      <span>{label}</span>
+      {selected && icon && <span>{icon}</span>}
+    </div>
+  );
+}
