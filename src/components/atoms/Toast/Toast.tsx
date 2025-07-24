8c0ebad821909a547ec832c71de3636863f1b779
ZeroIRC <save3172@gmail.com>
2025-07-25 00:36:10 +0900

    fix: Toast 메세지 width 개선

--- src/components/atoms/Toast/Toast.tsx
+++ src/components/atoms/Toast/Toast.tsx
@@ -32,10 +32,7 @@ export default function Toast({
   if (!isOpen) return null;
 
   return (
-    <div
-      className="fixed left-1/2 bottom-8 z-50 -translate-x-1/2 bg-surface-normal-bg06 text-texticon-onnormal-white rounded-[8px] flex items-center px-[20px] py-[16px] gap-[10px] transition-all duration-1000 ease-in-out animate-in fade-in slide-in-from-bottom-2"
-      style={{ minWidth: 328, maxWidth: "90vw" }}
-    >
+    <div className="fixed min-w-[328px] max-w-[568px] left-1/2 bottom-8 z-60 -translate-x-1/2 bg-surface-normal-bg06 text-texticon-onnormal-white rounded-[8px] flex items-center px-4 py-[16px] gap-[10px] transition-all duration-1000 ease-in-out animate-in fade-in slide-in-from-bottom-2 w-[calc(100vw-32px)]">
       {icon && <span className="mr-3">{icon}</span>}
       <span className="body-s-semibold">{message}</span>
     </div>
