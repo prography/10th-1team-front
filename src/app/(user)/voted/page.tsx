366547cc6fb645bb651eb9a2db659f2717f0987f
gominzip <ahaminji@naver.com>
2025-07-24 15:31:33 +0900

    refactor: 투표 활동 조회 api 연동 완료

--- src/app/(user)/voted/page.tsx
+++ src/app/(user)/voted/page.tsx
@@ -1,43 +1,21 @@
+import { getUserVotedActivity } from "@/apis/activity";
 import VotedPageTemplate from "@/components/templates/VotedPageTemplate/VotedPageTemplate";
-import { VotedActivityInfo } from "@/types/activity";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
-const dummy: VotedActivityInfo[] = [
-  {
-    place_id: "1",
-    place_name: "place_name",
-    category: "category",
-    platform: "naver",
-    reasons: [
-      "reason1",
-      "reason2",
-      "reason3",
-      "reason4",
-      "reason5",
-      "reason6",
-      "reason7",
-      "reason8",
-      "reason9",
-      "reason10",
-    ],
-    voted_date: "2025-07-07",
-  },
-  {
-    place_id: "2",
-    place_name: "place_name2",
-    category: "category2",
-    platform: "naver",
-    reasons: ["reason1", "reason2"],
-    voted_date: "2025-07-07",
-  },
-  {
-    place_id: "3",
-    place_name: "place_name3",
-    category: "category3",
-    platform: "kakao",
-    reasons: ["reason1", "reason2"],
-    voted_date: "2025-07-07",
-  },
-];
 export default async function VotedPage() {
-  return <VotedPageTemplate items={dummy} />;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["votedActivity"],
+    queryFn: getUserVotedActivity,
+  });
+  const dehydratedState = dehydrate(queryClient);
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <VotedPageTemplate />
+    </HydrationBoundary>
+  );
 }
