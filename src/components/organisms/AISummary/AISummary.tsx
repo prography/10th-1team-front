8d07d267c61ed148c3f54e95e9d6e30f80d2f18f
ZeroIRC <save3172@gmail.com>
2025-08-14 20:17:33 +0900

    feat: AI 요약에 다중 키워드 하이라이터 적용

--- src/components/organisms/AISummary/AISummary.tsx
+++ src/components/organisms/AISummary/AISummary.tsx
@@ -2,6 +2,8 @@
 
 import Tabs from "@/components/organisms/Tabs/Tabs";
 import BarGraph from "@/components/molecules/BarGraph/BarGraph";
+import MultiKeywordHighlighter from "@/components/atoms/MultiKeywordHighlighter/MultiKeywordHighlighter";
+import { colors } from "@/styles/colors";
 
 interface AISummaryProps {
   summaryAi: string;
@@ -19,13 +21,19 @@ export default function AISummary({ summaryAi, strengths }: AISummaryProps) {
     content: (
       <div className="flex flex-col w-full gap-[24px]">
         {summaryAi ? (
-          <p className="body-m-semibold px-[16px] pt-[12px] text-left">
-            {summaryAi}
-          </p>
+          <div className="body-m-semibold px-[16px] pt-[12px] text-left">
+            <MultiKeywordHighlighter
+              text={summaryAi}
+              highlights={[
+                { word: "카카오", color: colors.Brand.KaKao.Blue },
+                { word: "네이버", color: colors.Brand.Naver.Main },
+              ]}
+            />
+          </div>
         ) : (
-          <p className="body-m-semibold px-[16px] pt-[12px] text-texticon-onnormal-lowestemp text-left">
+          <span className="body-m-semibold px-[16px] pt-[12px] text-texticon-onnormal-lowestemp text-left">
             AI요약을 위한 리뷰 데이터가 부족해요
-          </p>
+          </span>
         )}
         <div className="flex justify-center">
           <BarGraph
